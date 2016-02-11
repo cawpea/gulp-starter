b19f174f151be68c1179d7d1a6f65d79aba1c9cc
Ohsumi Masaki <masakiohsumi@Ohsumi-no-MacBook-Pro.local>
2016-02-11 19:08:44 +0900

    add gulp-cache

--- tasks/sass.js
+++ tasks/sass.js
@@ -2,6 +2,7 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 var plumber = require('gulp-plumber');
 var notify = require('gulp-notify');
+var cache = require('gulp-cached');
 var browserSync = require('browser-sync');
 
 var paths = {
@@ -14,6 +15,7 @@ gulp.task('sass', function(){
 	var destGlob = paths.destDir + '/assets/css';
 
 	gulp.src(srcGlob)
+		.pipe(cache( 'sass' ))
 		.pipe(plumber({
 			errorHandler: notify.onError('Error: <%= error.message %>')
 		}))
